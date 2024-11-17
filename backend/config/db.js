a4b92de9fa44cc7d79b63d05be5ee124c25857a5
github-adie <adiemit23@gmail.com>
2024-11-17 10:38:18 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
- export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sudhanshusagar077:7277541915@cluster0.cynuwvv.mongodb.net/food-del').then(() =>console.log("DB Connected"));
-    
-}        
